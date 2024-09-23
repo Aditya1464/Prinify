748591d71a4119624279f296e1e7d0c846b23b14
Aditya Singh <adityasingh1464@gmail.com>
2024-09-23 22:44:31 +0530

    some error fix

--- task/src/LandingPage/robust/data.js
+++ task/src/LandingPage/robust/data.js
@@ -1,16 +1,16 @@
 let data = [
     {
-        "image": "../../public/higher-profits.svg", 
+        "image": "https://printify.com/pfh/assets/legacy/higher-profits.svg", 
         "heading": "Higher Profits",
         "text": "We offer some of the lowest prices in the industry because print providers continuously compete to win your business."
     },
     {
-        "image": "../../public/robust-scaling.svg", 
+        "image": "https://printify.com/pfh/assets/legacy/robust-scaling.svg", 
         "heading": "Robust Scaling",
         "text": "Easily handle peak holiday seasons, with our wide network of partners and automatic routing functionality."
     },
     {
-        "image": "../../public/best-selection.svg", 
+        "image": "https://printify.com/pfh/assets/legacy/best-selection.svg", 
         "heading": "Best Selection",
         "text": "With 900+ products and top quality brands, you can choose the best products for your business."
     }
